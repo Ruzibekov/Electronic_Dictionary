e95c16449173f4eb4d1c93969c82c36118ab8d90
shavkatbek <ruzibekov44@gmail.com>
2022-12-10 11:47:45 +0500

    Initial commit

---
+++ app/src/main/java/com/ruzibekov/electronicdictionary/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ruzibekov.electronicdictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
